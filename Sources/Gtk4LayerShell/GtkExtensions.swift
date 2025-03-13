7a5a58adfa397d9dba7b74db646a45b1e56120b7
WolfDan <5377526+WolfDan@users.noreply.github.com>
2025-03-12 21:50:21 -0500

    use raw pointers v2

--- Sources/Gtk4LayerShell/GtkExtensions.swift
+++ Sources/Gtk4LayerShell/GtkExtensions.swift
@@ -5,22 +5,23 @@ open class Display {
     public static func getMonitors() -> [OpaquePointer] {
         let list_model = get_g_list_monitors()
         var monitors: [OpaquePointer] = []
-        for i in 0..<list_model.count {
-            let monitor = list_model[i]
-            monitors.append(monitor)
+        let list_size = Int(g_list_model_get_n_items(list_model))
+        for i in 0..<list_size {
+            let monitor = OpaquePointer(g_list_model_get_item(list_model, guint(i)))
+            monitors.append(monitor!)
         }
         return monitors
     }
 
     public static func getMonitor(index: Int) -> OpaquePointer {
         let list_model = get_g_list_monitors()
-        if list_model.opaquePointer == nil {
-            fatalError("ListModel pointer is nil")
-        }
+        // if list_model.opaquePointer == nil {
+        //     fatalError("ListModel pointer is nil")
+        // }
 
         print("list_model setup")
 
-        let monitor: OpaquePointer? = list_model[index]
+        let monitor: OpaquePointer? = OpaquePointer(g_list_model_get_item(list_model, guint(index)))
 
         if monitor == nil {
             fatalError("Monitor pointer is nil")
@@ -31,7 +32,7 @@ open class Display {
         return monitor!
     }
 
-    static func get_g_list_monitors() -> GListModel {
+    static func get_g_list_monitors() -> OpaquePointer {
         let displayPointer = get_default()
         if displayPointer == nil {
             fatalError("Display not found! Ensure you are running a Wayland compositor")
@@ -46,7 +47,7 @@ open class Display {
 
         print("monitors setup!")
 
-        return GListModel(monitors!)
+        return monitors!
     }
 
     static func get_default() -> OpaquePointer? {
